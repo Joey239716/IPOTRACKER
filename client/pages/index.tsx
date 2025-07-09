861b4f9b1efec26ccbaf257ba1db42df2fdbef67
JoeyDaB3Ast <60456147+JoeyDaB3Ast@users.noreply.github.com>
2025-07-09 11:47:44 -0400

    added finnhub api key

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 function index() {
+
+  const [message, setMessage] = useState("Loading");
+  const [people, setPeople] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/home")
+    .then((response) => response.json())
+    .then((data) => {
+      setMessage(data.message);
+      setPeople(data.people);
+      console.log(data.people)
+    });
+  }, []);
+
   return (
-    <div>index</div>
+    <div>
+      <div>{message}</div>
+      <div>
+      {
+        people.map((person, index) => (
+          <div key = {index}>
+            {person}
+          </div>
+        ))
+      }
+      </div>
+    </div>
   )
 }
 
