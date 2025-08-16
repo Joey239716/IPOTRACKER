78a78773f27d51185dddfa9d15e0b1536d287a86
Joey <thisisjoeyliu@gmail.com>
2025-08-15 23:36:57 -0400

    made it update to today

--- server/prod/main.py
+++ server/prod/main.py
@@ -4,5 +4,8 @@ from zoneinfo import ZoneInfo
 from .pipeline.pipeline import Pipeline
 
 if __name__ == "__main__":
+    # Get today's date in Eastern Time (Toronto timezone)
     et_today = datetime.datetime.now(ZoneInfo("America/Toronto")).date().isoformat()
-    Pipeline().fetch_and_push("2025-08-12", "2025-08-12")
+    
+    # Use the same day as start and end
+    Pipeline().fetch_and_push(et_today, et_today)
