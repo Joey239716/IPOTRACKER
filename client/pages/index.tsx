0a68df83ea4b5b374e8cbdb769070987d3853a54
Joey <thisisjoeyliu@gmail.com>
2025-10-09 22:00:40 -0400

    Update index.tsx

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,49 +1,5 @@
-/* import React, { useEffect, useState } from 'react'
-
-
-function index() {
-
-  const [message, setMessage] = useState("Loading");
-  const [people, setPeople] = useState([])
-
-  useEffect(() => {
-    fetch("http://localhost:8080/api/home")
-    .then((response) => response.json())
-    .then((data) => {
-      setMessage(data.message);
-      setPeople(data.people);
-      console.log(data.people)
-    });
-  }, []);
-
-  return (
-    <div>
-      <div>{message}</div>
-      <div>
-      {
-        people.map((person, index) => (
-          <div key = {index}>
-            {person}
-          </div>
-        ))
-      }
-      </div>
-    </div>
-  )
-}
-
-export default index
-*/
-
 import MainPage from '@/components/mainPage'
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-        <MainPage />
-        {children}
-      </body>
-    </html>
-  )
+export default function HomePage() {
+  return <MainPage />
 }
